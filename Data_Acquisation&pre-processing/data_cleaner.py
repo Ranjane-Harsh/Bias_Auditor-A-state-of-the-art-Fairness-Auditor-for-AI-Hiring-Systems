668e29a792ed642fd2986a5c11b41b6c72c6863d
Harsh Ranjane <harsh.21910471@viit.ac.in>
2025-05-29 13:34:04 +0200

    Changes for Day 4

--- Data_Acquisation&pre-processing/data_cleaner.py
+++ Data_Acquisation&pre-processing/data_cleaner.py
@@ -2,6 +2,7 @@ from data_loader import load_csv_data, validate_dataframe, file_path
 import seaborn as sb
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 def handle_missing_values(df,subset_of_col,strategy):
     strategy = strategy.upper()
@@ -45,11 +46,18 @@ def encode_categorical_col(df,non_numeric_col):
     df = pd.get_dummies(df, columns = non_numeric_col, drop_first = True,dtype = float)
     return df
 
+def scale_numerical_col(df,numeric_col):
+    scaler = StandardScaler()
+    scaled_val = scaler.fit_transform(df[numeric_col])
+    scaled_df = pd.DataFrame(scaled_val, columns=numeric_col)
+
+    return scaled_df
+
 def main():
     data_frame = load_csv_data(file_path)
     non_numeric_col = ["gender","race","college_tier","education_level"]
     non_numeric_df = handle_missing_values(data_frame,non_numeric_col,"Drop")
-     
+    
     numeric_col = ["years_experience", "skills_score","interview_score","test_score"]
     non_null_df = handle_missing_values(non_numeric_df,numeric_col,"Median")
     print("The summary for non_empty dataframe looks like: ")
@@ -58,10 +66,8 @@ def main():
     outlier_free = eliminate_outliers(non_null_df)
     standardized_df = standardize_col(outlier_free)
     encoded_df= encode_categorical_col(standardized_df,non_numeric_col)
-    print(encoded_df.dtypes)
+    scaled_df = scale_numerical_col(encoded_df,numeric_col)
+    print(scaled_df[numeric_col].describe())
     
-    
-    
-
 if __name__ == "__main__":
     main()
